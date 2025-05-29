aba6dd3cda4bcde195bb6cf2d375f8e43d1111d7
unknown <jaycraftart@gmail.com>
2025-05-28 21:54:58 -0400

    Quartz sync: May 28, 2025, 9:54 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -44,20 +44,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer({
-      title: "Explorer", // title of the explorer component
-      folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-      folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
-      useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
-      // omitted but shown later
-      sortFn: (a, b) => {
-        return a.displayName.localeCompare(b.displayName)
-      },
-      filterFn: (node) => node.displayName !== "tags",
-      mapFn: undefined,
-      // what order to apply functions in
-      order: ["filter", "map", "sort"],
-    })
+    Component.Explorer()
   ],
   right: [],
 }
